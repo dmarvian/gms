a6aa13f1cfd925bca45e24e5bcc8dc479c57d395
Hideo Darell Marvian <dmarvian@gmail.com>
2025-11-02 18:57:47 +0700

    edit detail layanan

--- src/app/services/[id]/page.js
+++ src/app/services/[id]/page.js
@@ -1,7 +1,6 @@
-import Footer from "@/components/layout/footer/Footer";
+import Footer from "@/components/layout/footer/gms-footer";
 import Header from "@/components/layout/header/Header";
 import ServiceDetailsMain from "@/components/layout/main/ServiceDetailsMain";
-import Cta from "@/components/sections/cta/Cta";
 import BackToTop from "@/components/shared/others/BackToTop";
 import HeaderSpace from "@/components/shared/others/HeaderSpace";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
@@ -26,7 +25,6 @@ export default async function ServiceDetails({ params }) {
 					<main>
 						<HeaderSpace />
 						<ServiceDetailsMain currentItemId={parseInt(id)} />
-						<Cta />
 					</main>
 					<Footer />
 				</div>
